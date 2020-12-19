043ce58069fd62979962d9b8a80244814519980e
Ben Hayes <b.t.hayes@comcast.net>
2020-12-19 17:57:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season, holiday|
+  puts "#{season.to_s.capitalize}: #{holiday.to_s.capitalize}"
+end
 end
 
 
