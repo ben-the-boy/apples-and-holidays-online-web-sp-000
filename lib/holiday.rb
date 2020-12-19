9a2156c2c9e34c616afd147235ae51c0f60aa48b
Ben Hayes <b.t.hayes@comcast.net>
2020-12-19 18:13:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,11 +74,11 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize!}:"
       holiday.each do |day, supply_list|
-        binding.pry
         puts "#{day.to_s.capitalize!}:"
       end
       
   end
+  puts 
 end
 
 
