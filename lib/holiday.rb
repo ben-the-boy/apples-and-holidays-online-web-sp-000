e94822a283dbfa1190bc2d9a5f1a86b87ceee9f9
Ben Hayes <b.t.hayes@comcast.net>
2020-12-19 19:12:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,7 +94,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_with_bbq = []
+holidays_with_bbq = []
 holiday_hash.each do |season, holiday|
   holiday.each do |holiday, supplies|
     if supplies.include?("BBQ")
